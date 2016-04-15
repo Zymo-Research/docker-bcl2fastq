fe7592eb7e7f265180af02c30e8b92394805ed7f
deep639 <deep639@users.noreply.github.com>
2016-04-15 16:49:21 -0700

    Update run_bcl2fastq.sh
    modified to include use-bases mark

--- run_bcl2fastq.sh
+++ run_bcl2fastq.sh
@@ -7,6 +7,8 @@ configureBclToFastq.pl \
 --fastq-cluster-count 0 \
 --mismatches 1 \
 --with-failed-reads \
+#Modify this later
+--use-bases-mask Y251,Y8,Y251,Y8 \
 --force &&
 
 make -C /output/Unaligned/ -j $cpu_num
